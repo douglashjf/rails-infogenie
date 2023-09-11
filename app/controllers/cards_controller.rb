3b72426570711efcfc18b9fcd029ea7bc4afe7f5
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-09-11 12:52:53 +0800

    created 1st draft of CRUD, pending confirmation of summary model and if changes required

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,52 @@
+class CardsController < ApplicationController
+  before_action :set_cards, only: %i[show edit update destroy]
+
+  def index
+    @cards = Card.all
+  end
+
+  def show
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(params_cards)
+    @card.user = current_user
+
+    if @card.save
+      redirect_to card_path(@card)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @card.update(house_params)
+    if @card.save
+      redirect_to @card
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to cards_path, status: :see_other
+  end
+
+  private
+  def set_cards
+    @card = Card.find(params[:id])
+  end
+
+  def params_cards
+    params.require(:card).permit(:content)
+  end
+
+end
