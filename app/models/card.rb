31d705cca40af78989ad1aa8f25299c210f06711
Douglas Hsu <douglas.g.hsu@gmail.com>
2023-09-16 14:01:10 +0800

    just small changes to the category model, the actual filter html is not done yet

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,7 @@ class Card < ApplicationRecord
   belongs_to :user
   has_many :summaries
   has_many :favourites
+  CATEGORIES = %w[ Life Health Relationships Self Improvement Productivity Mindfulness Work Technology Blockchain Data Science Software Development Media Art Gaming Society Economics Education Equality Culture Philosophy Religion Spirituality World Nature Travel ]
 
   scope :active, -> { where(deleted_at: nil) }
 
