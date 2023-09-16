37de6b2722c7fd7283743523714e297830cb6b96
Tay Yi Yang <tayyiyang381@gmail.com>
2023-09-16 12:02:51 +0800

    yay

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,16 @@
 puts "Clearing old data"
 
 User.destroy_all
-Card.Destroy_all
-Summary.Destroy_all
+Card.destroy_all
+Summary.destroy_all
 
 # Create data
 
 puts "Creating new data"
 
 # Creating six users, cards and summaries.
-first_name = %w[Jacob Douglas Sean Yi Zong Yong]
-last_name = %w[Khong Hsu Wong Yang Han Cheng]
+first_name = %w[ Zong Yong]
+last_name = %w[ Yang Han Cheng]
 primary_keywords = %w[Crypto Music Marathon Karaoke Code]
 secondary_keywords = %w[Currency Theory Race Singing Wars]
 key_points = %W["Etherium is one of the main trading currencies"
@@ -35,30 +35,153 @@ key_questions = %W["What is the lowest price ethereum can go?"
                       "What is the most common language in coding?"]
 
 # Initialize the iteration variable
-i = 0
 
-6.times do
-  User.create!(
-    first_name: first_name[i],
-    last_name: last_name[i],
-    email: "#{first_name[i].downcase}@gmail.com",
-    password: "password"
-  )
-  puts "Created #{first_name[i]} #{last_name[i]}."
+# First User
+user_jacob = User.create(
+  first_name: "Jacob",
+  last_name: "Khong",
+  email: "jacob@gmail.com",
+  password: "password"
+)
+puts "Created Jacob Khong"
 
-  Card.create!(
-    user: User.all[i],
-    primary_keywords: primary_keywords[i],
-    secondary_keywords: secondary_keywords[i]
-  )
-  puts "Created card No. #{i + 1}."
+# Second User
+user_doug = User.create(
+  first_name: "Douglas",
+  last_name: "Hsu",
+  email: "doug@gmail.com",
+  password: "password"
+)
+puts "Created Douglas Hsu"
 
-  Summary.create!(
-    card: Card.all[i],
-    key_points: key_points[i],
-    key_questions: key_questions[i]
-  )
-  puts "Created summary No. #{i + 1}."
+# Third User
+user_sean = User.create(
+  first_name: "Sean",
+  last_name: "Wong",
+  email: "sean@gmail.com",
+  password: "password"
+)
+puts "Created Sean Wong"
 
-  i += 1
-end
+# Fourth User
+user_yiyang = User.create(
+  first_name: "Yi",
+  last_name: "Yang",
+  email: "yiyang@gmail.com",
+  password: "password"
+)
+puts "Created Yi Yang"
+
+# Fifth User
+user_yc = User.create(
+  first_name: "Yong",
+  last_name: "Cheng",
+  email: "yongcheng@gmail.com",
+  password: "password"
+)
+puts "Created YC"
+
+# Sixth User
+user_zonghan = User.create(
+  first_name: "Zong",
+  last_name: "Han",
+  email: "zonghan@gmail.com",
+  password: "password"
+)
+puts "Created Zong Han"
+
+# First Card
+card_1 = Card.create!(
+  user_id: user_zonghan.id,
+  primary_keywords: "Crypto",
+  secondary_keywords: "Currency"
+)
+puts "Created card 1"
+
+# Second Card
+card_2 = Card.create!(
+  user_id: user_doug.id,
+  primary_keywords: "Music",
+  secondary_keywords: "Theory"
+)
+puts "Created card 2"
+
+# Third Card
+card_3 = Card.create!(
+  user_id: user_doug.id,
+  primary_keywords: "Marathon",
+  secondary_keywords: "Race"
+)
+puts "Created card 3"
+
+# Fourth Card
+card_4 = Card.create!(
+  user_id: user_jacob.id,
+  primary_keywords: "Karaoke",
+  secondary_keywords: "Singing"
+)
+puts "Created card 4"
+
+# Fifth Card
+card_5 = Card.create!(
+  user_id: user_sean.id,
+  primary_keywords: "Code",
+  secondary_keywords: "Wars"
+)
+puts "Created card 5"
+
+# Sixth Card
+card_6 = Card.create!(
+  user_id: user_jacob.id,
+  primary_keywords: "Sports",
+  secondary_keywords: "Tennis"
+)
+puts "Created card 6"
+
+# First Summary
+Summary.create!(
+  card_id: card_1.id,
+  key_points: "Etherium is one of the main trading currencies",
+  key_questions: "What is the lowest price ethereum can go?"
+)
+puts "Created summary 1"
+
+# Second Summary
+Summary.create!(
+  card: card_2.id,
+  key_points: "Grade 5 music theory is compulsory to go higher",
+  key_questions: "What is the highest music theory grade?"
+)
+puts "Created summary 2"
+
+# Third Summary
+Summary.create!(
+  card: card_3.id,
+  key_points:  "42km is the distance of the full marathon",
+  key_questions: "What is the history of marathon?"
+)
+puts "Created summary 3"
+
+# Fourth Summary
+Summary.create!(
+  card: card_4.id,
+  key_points: "Very common way to de-stress",
+  key_questions: "What is the most common place for karaoke?"
+)
+puts "Created summary 4"
+
+# Fifth Summary
+Summary.create!(
+  card: card_5.id,
+  key_points: "Very good to improve coding skills",
+  key_questions: "What is the most common language in coding?"
+)
+puts "Created summary 5"
+
+# Sixth Summary
+Summary.create!(
+  card: card_6.id,
+  key_points: "There are 8 levels of coding",
+  key_questions: "What is the different languages they have?"
+)
+puts "Created summary 6"
