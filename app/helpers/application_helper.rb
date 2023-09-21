d5763eb70c3221fe9be0e02f9252e5e25f6124e0
Tay Yi Yang <tayyiyang381@gmail.com>
2023-09-21 18:29:52 +0800

    Share-button-whatsapp created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+# app/helpers/application_helper.rb
+
+def whatsapp_share_url
+  current_page_url = request.original_url
+  encoded_url = CGI.escape(current_page_url)
+  "https://api.whatsapp.com/send?text=#{encoded_url}"
+end
+
 end
