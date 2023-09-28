652e00bf6653fe63cdb1672245ed04d53eeeb557
Jacob Khong <jacobkhongdev@gmail.com>
2023-09-28 14:43:37 +0800

    added routes for sidekiq admin

--- app/jobs/news_refresh_job.rb
+++ app/jobs/news_refresh_job.rb
@@ -2,9 +2,25 @@ class NewsRefreshJob < ApplicationJob
   queue_as :default
 
   def perform
-    # Do something later
-    puts "im starting a job"
+    puts "I'm starting the fake job"
     sleep 3
-    puts "its done"
+    puts "OK I'm done now"
   end
 end
+
+
+# # 1) check ALL cards and retrieve cards that is favourited by user
+    # favored_cards = Card.joins(:favourites).distinct
+
+
+    # # 2) perform refresh logic on all favoured cards and find out each card users
+    # favored_cards.each do |card|
+    #   # refresh logic
+    #   card.refresh_articles
+    #   puts "cards article refresh"
+    #   # Actionailer
+    #   # card.favourites.each do |favourite|
+    #   #   user = favourite.user
+    #   #   NewsNotificationMailer.news_updated(user, card).deliver_now
+    #   # end
+    # end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
+  require "sidekiq/web"
+  authenticate :user, ->(user) { user.email == 'jacob@gmail.com' } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -18,9 +22,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  require "sidekiq/web"
-  authenticate :user, ->(user) { user.email == 'jacob@gmail.com' } do
-    mount Sidekiq::Web => '/sidekiq'
-  end
 end
