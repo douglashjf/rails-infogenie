70b3b386173200edd264d78e3618cf1860976c06
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-10-03 21:16:50 +0800

    committing

--- app/javascript/controllers/show_slider_controller.js
+++ app/javascript/controllers/show_slider_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["slides","dots"]
   connect() {
-
+    this.sliderTarget.style.transform = `translateX(0)`;
     this.dotsTarget.classList.add("active")
   }
 
@@ -32,6 +32,35 @@ export default class extends Controller {
     this.dotsTargets.forEach(dot => dot.classList.remove("active"));
     this.slidesTargets.forEach((s,i) => s.style.transform = `translateX(${110 * (i - this._curSlide)}%)`)
     document.querySelector(`.dots_dot[data-slide="${this._curSlide}"]`).classList.add("active");
+  }
+
+  swipe(event) {
+    if (event.deltaX > 50) {
+      // Swipe right
+      this.showPreviousSlide();
+    } else if (event.deltaX < -50) {
+      // Swipe left
+      this.showNextSlide();
+    }
+  }
+
+  showNextSlide() {
+    if (this._curSlide < this.slidesTargets.length - 1) {
+      this._curSlide++;
+      this.updateSlidePosition();
+    }
+  }
+
+  showPreviousSlide() {
+    if (this._curSlide > 0) {
+      this._curSlide--;
+      this.updateSlidePosition();
+    }
+  }
+
+  updateSlidePosition() {
+    const translateX = -this._curSlide * 100;
+    this.slidesTarget.style.transform = `translateX(${translateX}%)`;
   }
 
 }
