3d67b730fa464d1cf43d76163ec5cd81416af5c7
Jacob Khong <jacobkhongdev@gmail.com>
2023-10-04 09:49:45 +0800

    created swiper for cards carousel

---
+++ app/javascript/controllers/index_card_swiper_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus";
+import Swiper from "swiper/bundle";
+
+export default class extends Controller {
+  connect() {
+    // Initialize Swiper here
+    const swiper = new Swiper(this.element, {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      effect: "coverflow",
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+}
