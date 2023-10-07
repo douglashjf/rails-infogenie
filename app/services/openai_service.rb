af68ef57a8d29ea11609b507787f4f8ec37ddd5d
Douglas Hsu <douglas.g.hsu@gmail.com>
2023-10-07 11:49:55 +0800

    save

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -24,7 +24,7 @@ class OpenaiService
           ],
           temperature: 0.7,
           stream: false,
-					max_tokens: 1000 # might want to check this
+					max_tokens: 2000 # might want to check this
       })
     # you might want to inspect the response and see what the api is giving you
     response_content = response["choices"][0]["message"]["content"]
