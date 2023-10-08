83e79c618dd6ee99eb542ea159b73ddf6c2497cc
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-10-08 14:38:37 +0800

    updated funfacts and added LoadingText plus updated format of loading Text

--- app/javascript/controllers/fun_fact_controller.js
+++ app/javascript/controllers/fun_fact_controller.js
@@ -2,34 +2,57 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="fun-fact"
 export default class extends Controller {
-  static targets = ["factText"]
+  static targets = ["factText","loadingText"]
 
   connect() {
     const funFacts = [
-      "Remember, while making wishes can be fun and inspirational, it's also important to take action to turn those wishes into reality. Wishing is just the first step toward achieving your dreams!",
-      "Your elevator pitch should be concise, typically under 30 seconds, to effectively introduce yourself and your business.",
-      "A firm and confident handshake can leave a positive impression and build trust during networking events.",
-      "Networking can happen anywhere, from coffee shops to social media platforms, so always be prepared to make connections.",
-      "The Rule of 150: Anthropologist Robin Dunbar proposed the idea that individuals can maintain stable social relationships with around 150 people. Consider this when managing your network.",
-      "There's often a comical dance at networking events where everyone circles the room, trying to find the best conversations to join.",
-      "Networking breakups can be funny when you start making up excuses like 'I have to refill my coffee' to politely exit a conversation, when in fact you're just finding an excuse to use Infogenie",
-      "Trends often lead to innovation. Being aware of the latest trends can inspire creativity and lead to the development of new ideas and products.",
-      "Informed decision-making relies on accurate and current information. Staying updated helps you make smarter choices, whether in business or daily life.",
-      "When you're well-informed, you can engage in meaningful conversations with others, making you a more interesting and valuable contact in your professional and social networks.",
-      "Staying informed is a lifelong learning journey. It keeps your mind active and curious, contributing to personal growth."
-
+      // "Remember, while making wishes can be fun and inspirational, it's also important to take action to turn those wishes into reality. Wishing is just the first step toward achieving your dreams!",
+      // "Your elevator pitch should be concise, typically under 30 seconds, to effectively introduce yourself and your business.",
+      // "A firm and confident handshake can leave a positive impression and build trust during networking events.",
+      // "Networking can happen anywhere, from coffee shops to social media platforms, so always be prepared to make connections.",
+      // "The Rule of 150: Anthropologist Robin Dunbar proposed the idea that individuals can maintain stable social relationships with around 150 people. Consider this when managing your network.",
+      // "There's often a comical dance at networking events where everyone circles the room, trying to find the best conversations to join.",
+      // "Networking breakups can be funny when you start making up excuses like 'I have to refill my coffee' to politely exit a conversation, when in fact you're just finding an excuse to use Infogenie",
+      // "Trends often lead to innovation. Being aware of the latest trends can inspire creativity and lead to the development of new ideas and products.",
+      // "Informed decision-making relies on accurate and current information. Staying updated helps you make smarter choices, whether in business or daily life.",
+      // "When you're well-informed, you can engage in meaningful conversations with others, making you a more interesting and valuable contact in your professional and social networks.",
+      // "Staying informed is a lifelong learning journey. It keeps your mind active and curious, contributing to personal growth."
+      "Based on your inputs, we feed your keywords, guided by a set of targeted prompts into text generation AI-powered application: ChatGPT. This returns an output which is further streamlined into the summary of points and questions users receive.",
+      "In case you're wondering, our images arn't hand-drawn either. Instead, we are integrated with the AI image generation service Dall-E to produce an abstract interpretation of the keywords submitted.",
+      "By interacting with relevant news sources online and extracting the latest updates via the form of an API, the world's information is now at your fingertips.",
+      "Also, from the user's inputs, our smart engine automatically categorizes and sorts created cards into our curated categories without users having to lift a finger",
+      "This seamless interweaving of tasks and logic happens in the background while we shop for a magic carpet to wrap everything into this format to present you your desired output - bite-sized smart updates."
     ];
 
+    const loadingProgress = [
+      "Generating Summaries...", "Generating Images...", "Extracting Relevant News...", "Auto-categorizing card...", "Granting your wish..."
+    ];
 
+    let index = 0;
     const displayRandomFunFact = () => {
       console.log('crazy');
-      const randomIndex = Math.floor(Math.random() * funFacts.length);
-      const funFactText = funFacts[randomIndex];
+      if (index === funFacts.length - 1) index = 0;
+      index ++;
+      // const randomIndex = Math.floor(Math.random() * funFacts.length);
+      const funFactText = funFacts[index];
       this.factTextTarget.innerText = funFactText;
     }
 
-    displayRandomFunFact();
+    let i = 0;
+    const displayLoading = () => {
+      if (i === loadingProgress.length - 1) i = 0;
+      i ++;
+      // const randomi = Math.floor(Math.random() * funFacts.length);
+      const loadingText = loadingProgress[i];
+      this.loadingTextTarget.innerText = loadingText;
+    }
 
-    setInterval(displayRandomFunFact, 5000);
+
+
+    displayRandomFunFact();
+    displayLoading();
+
+    setInterval(displayRandomFunFact, 9000);
+    setInterval(displayLoading, 14000);
   }
   }
